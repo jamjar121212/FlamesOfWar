da821310fb9f1194837a5ae6e722163a5010d11c
James Connelly <jamesc1992@hotmail.com>
2015-02-07 13:53:31 +0000

    Added Scripts folder and the start of an input manager and piece selection script.

---
+++ Assets/Custom Assets/Scripts/InputManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+	public bool isMouseDown = false;
+	public PieceSelection pieceSelection;
+
+	void Start () {
+		this.pieceSelection = this.gameObject.GetComponent<PieceSelection>();
+		Debug.Log ("initialising input manager...");
+	}
+	
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			isMouseDown = true;
+			this.SelectObject();
+		}
+
+		if (Input.GetMouseButtonUp(0)) {
+			isMouseDown = false;
+		}
+	}
+
+	void SelectObject() {
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if(Physics.Raycast(ray, out hit)) 
+		{
+			Debug.Log("Clicked Something!");
+		}
+	}
+}
